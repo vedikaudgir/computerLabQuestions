bb203e9c1d941ad993aaaf9bd11d4ad8cfb3d055
Vedika Udgir <vedikaudgir@gmail.com>
2024-11-30 15:31:25 +0530

    Add files via upload

---
+++ lab11/problem1/studentData.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+struct studentData
+{
+    char studentName[30];
+    char department[5];
+    char course[10];
+    int MISnumber;
+    int joiningYear;
+};
+
+void readStudentData(struct studentData structarray[5])
+{
+    int i;
+    for (i = 0; i < 5; i++)
+    {
+        printf("\n\nEnter deatils of student %d",i + 1);
+
+        printf("\n\nEnter Student Name - ");
+        getchar();
+        scanf("%[^\n]", structarray[i].studentName);
+
+        printf("\nEnter MIS Nnmber - ");
+        getchar();
+        scanf("%d", &structarray[i].MISnumber);
+
+        printf("\nEnter department - ");
+        getchar();
+        scanf("%[^\n]", structarray[i].department);
+
+        printf("\nEnter course - ");
+        getchar();
+        scanf("%[^\n]", structarray[i].course);
+
+        printf("\nEnter joining year - ");
+        scanf("%d", &structarray[i].joiningYear);
+    }
+}
+
+void printStudentData(struct studentData structarray[5])
+{
+    int i;
+    for (i = 0; i < 5; i++)
+    {
+        printf("\n\nData of Student %d",i + 1);
+
+        printf("\n\n Student Name - %s",structarray[i].studentName);
+        printf("\n MIS Nnmber - %d",structarray[i].MISnumber);
+        printf("\n department - %s",structarray[i].department);
+        printf("\n course - %s",structarray[i].course);
+        printf("\n joining year - %d",structarray[i].joiningYear);
+    }
+}
+void main()
+{
+    struct studentData a[5];
+    readStudentData(a);
+    printStudentData(a);
+}
